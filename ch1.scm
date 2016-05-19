7d6af0578622fc9e9da0d4209a718674217cae37
Jeff Gonis <jeffgonis@fastmail.com>
2016-05-19 16:37:59 -0600

    messing around with getting unit tests set up and starting on ch1

--- ch1.scm
+++ ch1.scm
@@ -1,1 +1,11 @@
-(+ 1 1)
+(define-library (sicp ch1)
+  (export )
+  (import (scheme base))
+  (begin
+    ))
+
+(import (srfi :78))
+(import (sicp ch1))
+(check-set-mode! 'summary)
+(check-report)
+(check-reset!)
