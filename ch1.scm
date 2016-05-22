4e3d99e3c9c6edcbd746da76a3cebf6964ff13af
Jeff Gonis <jeff.gonis@gmail.com>
2016-05-21 22:50:27 -0600

    Create square and sum-of-squares procedures and a few basic tests for them

--- ch1.scm
+++ ch1.scm
@@ -1,11 +1,18 @@
 (define-library (sicp ch1)
-  (export )
-  (import (scheme base))
+  (export square sum-of-squares)
+  (import (except (scheme base) square))
   (begin
-    ))
+    (define square
+      (lambda (n)
+        (* n n)))
+    (define sum-of-squares
+      (lambda (x y)
+        (+ (square x) (square y))))))
 
 (import (srfi :78))
-(import (sicp ch1))
+(import (rename (prefix (sicp ch1) sicp-)))
 (check-set-mode! 'summary)
+(check (sicp-square 5) => 25)
+(check (sicp-sum-of-squares 3 4) => 25)
 (check-report)
 (check-reset!)
