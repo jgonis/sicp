9177df0164fc31dee793bebaebef1a51dd1829a9
Jeff Gonis <jeffgonis@fastmail.com>
2016-08-30 21:59:10 -0600

    Implement rational arithmetic system as per chapter 2.1.1

--- ch2.scm
+++ ch2.scm
@@ -1,1 +1,42 @@
 (include "ch1.scm")
+
+(define-library (sicp ch211)
+  (export make-rational-number
+          add-rational-numbers
+          subtract-rational-numbers
+          multiply-rational-numbers
+          divide-rational-numbers
+          rational-numbers-equal?
+          print-rational-number)
+  (import (scheme base)
+          (scheme write))
+  (begin
+    (define (add-rational-numbers x y)
+      (make-rational-number (+ (* (numer x) (denom y))
+                               (* (numer y) (denom x)))
+                            (* (denom x) (denom y))))
+    (define (subtract-rational-numbers x y)
+      (make-rational-number (- (* (numer x) (denom y))
+                               (* (numer y) (denom x)))
+                            (* (denom x) (denom y))))
+    (define (multiply-rational-numbers x y)
+      (make-rational-number (* (numer x) (numer y))
+                            (* (denom x) (denom y))))
+    (define (divide-rational-numbers x y)
+      (make-rational-number (* (numer x) (denom y))
+                            (* (denom x) (numer y))))
+    (define (rational-numbers-equal? x y)
+      (= (* (numer x) (denom y))
+         (* (numer y) (denom x))))
+    (define (make-rational-number numer denom)
+      (let ((g (gcd numer denom)))
+        (cons (/ numer g)
+              (/ denom g))))
+    (define (numer rational-number)
+      (car rational-number))
+    (define (denom rational-number)
+      (cdr rational-number))
+    (define (print-rational-number rational-number)
+      (display (numer rational-number))
+      (display "/")
+      (display (denom rational-number)))))
