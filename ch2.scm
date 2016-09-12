1a1267458cda443cc005548da4ba2cd23e17211e
Jeff Gonis <jeffgonis@fastmail.com>
2016-09-12 08:00:31 -0600

    Ex 2.8 and 2.9

--- ch2.scm
+++ ch2.scm
@@ -186,7 +186,7 @@
           *-interval
           /-interval
           subtract-interval
-          )
+          interval-width)
   (import (scheme base)
           (scheme write))
   (begin
@@ -217,5 +217,20 @@
                    (upper-bound i2))))
         (make-interval (min p1 p2 p3 p4)
                        (max p1 p2 p3 p4))))
-    (define (/-interval i1 i2) 1)
-    (define (subtract-interval i1 i2) 1)))
+    (define (/-interval i1 i2)
+      (*-interval i1
+                  (make-interval (/ 1 (upper-bound i2))
+                                 (/ 1 (lower-bound i2)))))
+    (define (subtract-interval i1 i2)
+      (let ((p1 (- (lower-bound i1)
+                   (lower-bound i2)))
+            (p2 (- (lower-bound i1)
+                   (upper-bound i2)))
+            (p3 (- (upper-bound i1)
+                   (lower-bound i2)))
+            (p4 (- (upper-bound i1)
+                   (upper-bound i2))))
+        (make-interval (min p1 p2 p3 p4)
+                       (max p1 p2 p3 p4))))
+    (define (interval-width interval)
+      (/ (abs (- (upper-bound interval) (lower-bound interval))) 2))))
