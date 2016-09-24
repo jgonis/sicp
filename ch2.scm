aa3b8a48579cdf67303283e0f8f0b95660fbce4c
Jeff Gonis <jeffgonis@fastmail.com>
2016-09-23 20:06:13 -0600

    Exercise 2.25

--- ch2.scm
+++ ch2.scm
@@ -407,3 +407,7 @@
 ;;               3   4
 
 ;;Exercise 2.25
+;;(car (cdr (car (cdr (cdr '(1 3 (5 7) 9))))))
+;;(car (car ((7))))
+;;(car (cdr (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr
+;;    '(1 (2 (3 (4 (5 (6 7)))))))))))))))))
