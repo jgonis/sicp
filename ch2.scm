d16e33d923550874dbef9907678218927e654595
Jeff Gonis <jeffgonis@fastmail.com>
2016-12-21 13:27:48 -0700

    First implementation of sets as unordered lists.

--- ch2.scm
+++ ch2.scm
@@ -901,3 +901,28 @@
               (make-product (deriv (multiplier exp) var)
                             (multiplicand exp))))
             (else (error "unknown expression type!"))))))
+
+(define-library (sicp ch233)
+  (export union-set
+          intersection-set
+          element-of-set?
+          adjoin-set)
+  (import (scheme base))
+  (begin
+    (define (union-set set1 set2)
+      (cond ((null? set1) set2)
+            ((element-of-set? (car set1) set2)
+             (union-set (cdr set1) set2))
+            (else (union-set (cdr set1) (cons (car set1) set2)))))
+    (define (intersection-set set1 set2)
+      (cond ((or (null? set1) (null? set2)) '())
+            ((element-of-set? (car set1) set2)
+             (cons (car set1) (intersection-set (cdr set1) set2)))
+            (else (intersection-set (cdr set1) set1))))
+    (define (element-of-set? x set)
+      (cond ((null? set) #f)
+            ((equal? x (car set)) #t)
+            (else (element-of-set? x (cdr set)))))
+    (define (adjoin-set x set)
+      (cond ((element-of-set? x set) set)
+            (else (cons x set))))))
