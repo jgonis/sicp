ce02eaec41535b1cdba3a01e38e7c1a72781a5c3
Jeff Gonis <jeffgonis@fastmail.com>
2017-01-03 22:41:10 -0700

    Basic tree functionality

--- utils.scm
+++ utils.scm
@@ -9,3 +9,19 @@
       (* x x x))
      (define (average x y)
       (/ (+ x y) 2))))
+
+(define-library (sicp tree-lib)
+  (export node
+          left-branch
+          right-branch
+          make-tree)
+  (import (scheme base))
+  (begin
+    (define (node tree)
+      (car tree))
+    (define (left-branch tree)
+      (car (cdr tree)))
+    (define (right-branch tree)
+      (car (cdr (cdr tree))))
+    (define (make-tree node left right)
+      (list node left right))))
