8d2c5ebca7026054b0e1da0cfd7024b8c1d9521a
Jeff Gonis <jeffgonis@fastmail.com>
2017-01-31 22:48:00 -0700

    Initial work on huffman coding various files

---
+++ huffman.scm
@@ -0,0 +1,23 @@
+(include "Multisets.sld")
+(import (scheme file))
+(import (scheme write))
+(import (scheme process-context))
+
+(define (open-file)
+  (define (output-contents contents)
+    (define (output-helper output-port)
+      (write-string contents output-port))
+      (let ((output-file "c:/Users/jeffg/Code/sicp/test2.txt"))
+        (call-with-output-file output-file output-helper)))
+  (define (input-func input-port)
+    (define (input-helper input-port contents length)
+      (let ((current-char (read-char input-port)))
+        (cond ((eof-object? current-char) (output-contents contents))
+              (else (input-helper input-port
+                            (string-append contents
+                                           (string current-char))
+                            (+ length 1))))))
+    (input-helper input-port (make-string 0) 0))
+  (let ((input-file "c:/Users/jeffg/Code/sicp/test.txt"))
+    (cond ((file-exists? input-file)
+           (call-with-input-file input-file input-func)))))
