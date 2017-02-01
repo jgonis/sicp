313b2e91120e82df460889ce95dac7f806a8ee89
Jeff Gonis <jeffgonis@fastmail.com>
2017-02-01 07:54:51 -0700

    Pull Huffman code into own library and better name for main func file

--- huffman.scm
+++ huffman.scm
@@ -3,21 +3,118 @@
 (import (scheme write))
 (import (scheme process-context))
 
-(define (open-file)
-  (define (output-contents contents)
-    (define (output-helper output-port)
-      (write-string contents output-port))
-      (let ((output-file "c:/Users/jeffg/Code/sicp/test2.txt"))
-        (call-with-output-file output-file output-helper)))
-  (define (input-func input-port)
-    (define (input-helper input-port contents length)
-      (let ((current-char (read-char input-port)))
-        (cond ((eof-object? current-char) (output-contents contents))
-              (else (input-helper input-port
-                            (string-append contents
-                                           (string current-char))
-                            (+ length 1))))))
-    (input-helper input-port (make-string 0) 0))
-  (let ((input-file "c:/Users/jeffg/Code/sicp/test.txt"))
-    (cond ((file-exists? input-file)
-           (call-with-input-file input-file input-func)))))
+(define-library (sicp huffman-base)
+  (export make-leaf
+          leaf?
+          leaf-symbol
+          leaf-weight
+          left-tree
+          right-tree
+          tree-symbols
+          tree-weight)
+  (import (scheme base)
+          (scheme cxr))
+  (begin
+    (define (make-leaf symbol weight)
+      (list 'leaf symbol weight))
+    (define (leaf? node)
+      (eq? (car node) 'leaf))
+    (define (leaf-symbol leaf)
+      (cadr leaf))
+    (define (leaf-weight leaf)
+      (caddr leaf))
+    (define (left-tree tree)
+      (car tree))
+    (define (right-tree tree)
+      (cadr tree))
+    (define (tree-symbols tree)
+      (cond ((leaf? tree) (list (leaf-symbol tree)))
+            (else (caddr tree))))
+    (define (tree-weight tree)
+      (cond ((leaf? tree) (leaf-weight tree))
+            (else (cadddr tree))))))
+
+(define-library (sicp huffman-encoding)
+  (export make-code-tree
+          adjoin-set
+          make-leaf-set
+          encode)
+  (import (scheme base)
+          (sicp huffman-base))
+  (begin
+    (define (make-code-tree left-tree right-tree)
+      (list left-tree
+            right-tree
+            (append (tree-symbols left-tree)
+                    (tree-symbols right-tree))
+            (+ (tree-weight left-tree)
+               (tree-weight right-tree))))
+    (define (adjoin-set x set)
+      (cond ((null? set) (list x))
+            ((< (tree-weight x) (tree-weight (car set)))
+             (cons x set))
+            (else (cons (car set) (adjoin-set x (cdr set))))))
+    
+    (define (make-leaf-set pairs)
+      (cond ((null? pairs) '())
+            (else (let ((pair (car pairs)))
+                    (adjoin-set (make-leaf (car pair)
+                                                   (cadr pair))
+                                        (make-leaf-set (cdr pairs)))))))
+    (define (encode message tree)
+      (cond ((null? message) '())
+            (else (append (encode-symbol (car message)
+                                         tree)
+                          (encode (cdr message)
+                                  tree)))))
+    (define (encode-symbol symbol tree)
+      (define (go-left? symbol symbol-list)
+        (cond ((null? symbol-list) #f)
+              ((eq? symbol (car symbol-list)) #t)
+              (else (go-left? symbol (cdr symbol-list)))))
+      (cond ((leaf? tree) (cond ((eq? (leaf-symbol tree) symbol) '())
+                                (else (error "unknown symbol!"))))
+            ((go-left? symbol (tree-symbols (left-tree tree)))
+             (cons 0 (encode-symbol symbol (left-tree tree))))
+            (else (cons 1 (encode-symbol symbol (right-tree tree))))))))
+
+(define-library (sicp huffman-decoding)
+  (export decode)
+  (import (scheme base)
+          (sicp huffman-base))
+  (begin
+    (define (decode bits tree)
+      (define (choose-branch bit tree)
+        (cond ((= bit 0) (left-tree tree))
+              ((= bit 1) (right-tree tree))
+              (else (error "bad bit!"))))
+      (define (decode-1 bits current-branch)
+        (cond ((null? bits) '())
+              (else (let ((next-branch (choose-branch (car bits)
+                                                      current-branch)))
+                      (cond ((leaf? next-branch)
+                             (cons (leaf-symbol next-branch)
+                                   (decode-1 (cdr bits) tree)))
+                            (else (decode-1 (cdr bits)
+                                            next-branch)))))))
+      (decode-1 bits tree))))
+
+
+(define-library (sicp generate-huffman)
+  (export generate-huffman-tree)
+  (import (scheme base)
+          (sicp huffman-encoding)
+          (sicp huffman-base))
+  (begin
+    (define (generate-huffman-tree pairs)
+      (define (successive-merge pairs)
+        (cond ((= (length pairs) 1) (car pairs))
+              (else (let ((first (car pairs))
+                          (second (cadr pairs))
+                          (rest (cddr pairs)))
+                      (successive-merge (adjoin-set
+                                         (make-code-tree first
+                                                         second)
+                                         rest))))))
+      (successive-merge
+       (make-leaf-set pairs)))))
