b93b5e4982f9b2b38a118f997883e7ce875c8ed5
Jeff Gonis <jeffgonis@fastmail.com>
2017-02-07 08:42:26 -0700

    Complete complex number example from 2.4.2

--- ch2.scm
+++ ch2.scm
@@ -1289,10 +1289,65 @@
       (cond ((pair? datum) (cdr datum))
             (else (error "Bad tagged datum: CONTENTS" datum))))))
 
+(define-library (sicp complex-num-lib-rectangular)
+  (export real-part-rectangular
+          imag-part-rectangular
+          magnitude-rectangular
+          angle-rectangular
+          make-from-real-imag-rectangular
+          make-from-mag-angle-rectangular)
+  (import (scheme base)
+          (scheme inexact)
+          (sicp type-tags))
+  (begin
+    (define (real-part-rectangular z)
+      (car z))
+    (define (imag-part-rectangular z)
+      (cdr z))
+    (define (magnitude-rectangular z)
+      (sqrt (+ (square (real-part-rectangular z))
+               (square (imag-part-rectangular z)))))
+    (define (angle-rectangular z)
+      (atan (imag-part-rectangular z)
+            (real-part-rectangular z)))
+    (define (make-from-real-imag-rectangular x y)
+      (attach-tag 'rectangular (cons x y)))
+    (define (make-from-mag-angle-rectangular r a)
+      (attach-tag 'rectangular
+                  (cons (* r (cos a))
+                        (* r (sin a)))))))
+
+(define-library (sicp complex-num-lib-polar)
+  (export real-part-polar
+          imag-part-polar
+          magnitude-polar
+          angle-polar
+          make-from-real-imag-polar
+          make-from-mag-angle-polar)
+  (import (scheme base)
+          (scheme inexact)
+          (sicp type-tags))
+  (begin
+    (define (real-part-polar z)
+      (* (magnitude-polar z)
+         (cos (angle-polar z))))
+    (define (imag-part-polar z)
+      (* (magnitude-polar z)
+         (sin (angle-polar z))))
+    (define (magnitude-polar z)
+      (car z))
+    (define (angle-polar z)
+      (cdr z))
+    (define (make-from-real-imag-polar x y)
+      (attach-tag 'polar
+                  (cons (sqrt (+ (square x) (square y)))
+                        (atan y x))))
+    (define (make-from-mag-angle-polar r a)
+      (attach-tag 'polar
+                  (cons r a)))))
+
 (define-library (sicp complex-num-lib)
-  (export rectangular?
-          polar?
-          add-complex
+  (export add-complex
           sub-complex
           mul-complex
           div-complex
@@ -1304,9 +1359,11 @@
           make-from-mag-angle)
   (import (scheme base)
           (scheme inexact)
-          (sicp type-tags))
+          (sicp type-tags)
+          (sicp complex-num-lib-rectangular)
+          (sicp complex-num-lib-polar))
   (begin
-    (define  (rectangular? z)
+    (define (rectangular? z)
       (eq? (type-tag z) 'rectangular))
     (define (polar? z)
       (eq? (type-tag z) 'polar))
@@ -1323,15 +1380,29 @@
       (make-from-mag-angle (/ (magnitude z1) (magnitude z2))
                            (- (angle z1) (angle z2))))
     (define (real-part z)
-      (car z))
+      (cond ((rectangular? z)
+             (real-part-rectangular (contents z)))
+            ((polar? z)
+             (real-part-polar (contents z)))
+            (else (error "Unknown type " z))))
     (define (imag-part z)
-      (cdr z))
+      (cond ((rectangular? z)
+             (imag-part-rectangular (contents z)))
+            ((polar? z)
+             (imag-part-polar (contents z)))
+            (else (error "Unknown type " z))))
     (define (magnitude z)
-      (sqrt (+ (* (real-part z) (real-part z))
-               (* (imag-part z) (imag-part z)))))
+      (cond ((rectangular? z)
+             (magnitude-rectangular (contents z)))
+            ((polar? z)
+             (magnitude-polar (contents z)))))
     (define (angle z)
-      (atan (imag-part z) (real-part z)))
+      (cond ((rectangular? z)
+             (angle-rectangular (contents z)))
+            ((polar? z)
+             (angle-polar (contents z)))
+            (else (error "Unknown type " z))))
     (define (make-from-real-imag x y)
-      (cons x y))
+      (make-from-real-imag-rectangular x y))
     (define (make-from-mag-angle r a)
-      (cons (* r (cos a)) (* r (sin a))))))
+      (make-from-mag-angle-polar r a))))
