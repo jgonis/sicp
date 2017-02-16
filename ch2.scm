550163480a44d4ab68bf9df086b9652b6fb6ea0e
Jeff Gonis <jeffgonis@fastmail.com>
2017-02-15 17:46:29 -0700

    Experimenting with generic methods from ch 2.4

--- ch2.scm
+++ ch2.scm
@@ -1,3 +1,4 @@
+
 (include "ch1.scm")
 (include "utils.scm")
 (include "tree.sld")
@@ -1426,3 +1427,106 @@
 ;;have a system that is adding new types more often and data directed
 ;;is more appropriate when you have a system that is adding new generic
 ;;methods more often.
+
+
+(define-library (sicp generic-lib)
+  (export get-func
+          put-func
+          get-func-list
+          reset-func-list
+          apply-generic)
+  (import (scheme base)
+          (sicp type-tags))
+  (begin
+    (define func-list '())
+    (define (get-func-list) func-list)
+    (define (get-func op type)
+      (define (helper op type func-list)
+        (cond ((null? func-list) #f)
+              ((and (eq? op (car (car (car func-list))))
+                    (equal? type (cdr (car (car func-list)))))
+               (cdr (car func-list)))
+              (else (helper op type (cdr func-list)))))
+      (helper op type func-list))
+    (define (put-func op type func)
+      (set! func-list (cons (cons (cons op type) func) func-list)))
+    (define (reset-func-list)
+      (set! func-list '()))
+    (define (apply-generic op . args)
+      (let* ((type-tags (map type-tag args))
+             (proc (get-func op type-tags)))
+        (cond (proc (apply proc (map contents args)))
+              (else (error "No method for these types" (list op type-tags))))))))
+
+(define-library (sicp complex-generic-ops)
+  (export make-from-real-imag
+          make-from-mag-ang
+          jreal-part
+          jimag-part
+          jmagnitude
+          jangle)
+  (import (scheme-base)
+          (sicp generic-lib))
+  (begin
+    (define (make-from-real-imag x y)
+      ((get-func 'make-from-real-imag 'rectangular) x y))
+    (define (make-from-mag-ang r a)
+      ((get-func 'make-from-mag-ang 'polar) r a))
+    (define (jreal-part z)
+      (apply-generic 'jreal-part z))
+    (define (jimag-part z)
+      (apply-generic 'jimag-part z))
+    (define (jmagnitude z)
+      (apply-generic 'jmagnitude z))
+    (define (jangle z)
+      (apply-generic 'jangle z))))
+
+(define (sicp complex-num-lib-polarv2)
+  (export install-polar-package)
+  (import (scheme base)
+          (scheme inexact)
+          (sicp type-tags)
+          (sicp generic-lib))
+  (define (install-polar-package)
+    (define (magnitude z) (car z))
+    (define (angle z) (cdr z))
+    (define (make-from-mag-angle r a)
+      (cons r a))
+    (define (real-part z)
+      (* (magnitude z) (cos (angle z))))
+    (define (imag-part z)
+      (* (magnitude z) (sin (angle z))))
+    (define (make-from-real-imag x y)
+      (cons (sqrt (+ (square x) (square y)))
+            (atan y x)))
+    (define (tag x) (attach-tag 'polar x))
+    (put-func 'real-part '(polar) real-part)
+    (put-func 'imag-part '(polar) imag-part)
+    (put-func 'magnitude '(polar) magnitude)
+    (put-func 'angle '(polar) angle)
+    (put-func 'make-from-real-imag 'polar (lambda (x y)
+                                            (tag (make-from-real-imag x y))))
+    (put-func 'make-from-mag-ang 'polar (lambda (r a) (tag (make-from-mag-ang r a))))
+    'done))
+
+(define-library (sicp complex-num-lib-v2)
+  (export add-complex
+          sub-complex
+          mul-complex
+          div-complex)
+  (import (scheme base))
+  (begin
+    (define (add-comple z1 z2)
+      (make-from-real-imag (+ (jreal-part z1) (jreal-part z2))
+                           (+ (jimag-part z1) (jimag-part z2))))
+    (define (sub-complex z1 z2)
+      (make-from-real-image (- (jreal-part z1) (jreal-part z2))
+                            (- (jimag-part z1) (jimag-part z2))))
+    (define (mul-complex z1 z2)
+      (make-from-mag-angle (* (jmagnitude z1) (jmagnitude z2))
+                           (+ (jangle z1) (jangle z2))))
+    (define (div-complex z1 z2)
+      (make-from-mag-ang (/ (jmagnitude z1) (jmagnitude z2))
+                         (- (jangle z1) (jangle z2))))))
+
+
