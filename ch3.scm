e76ec0b4a52e95c4dd26a2e6c0db1ce8f0786bde
Jeff Gonis <jeffgonis@fastmail.com>
2017-02-23 13:58:08 -0700

    Complete exercise 3.1

--- ch3.scm
+++ ch3.scm
@@ -1,1 +1,9 @@
 (include "ch2.scm")
+(define-library (sicp ex31)
+  (export ex31)
+  (import (scheme base))
+  (begin
+    (define (ex31 n)
+      (lambda (x)
+        (set! n (+ n x))
+        n))))
