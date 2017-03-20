543dd2324ebb1f1125faae0aaef0a51aba050315
Jeff Gonis <jeffgonis@fastmail.com>
2017-03-20 07:59:22 -0600

    A bit more work on parsing the input file into a tree of char occurences

--- EncodeFiles.scm
+++ EncodeFiles.scm
@@ -21,3 +21,17 @@
   (let ((input-file "./testinputs/test.txt"))
     (cond ((file-exists? input-file)
            (call-with-input-file input-file input-func)))))
+
+(define (parse-file)
+  (define (input-func input-port)
+    (define (input-helper input-port contents)
+      (let ((current-char (read-char input-port)))
+        (cond ((eof-object? current-char) contents)
+              (else (input-helper
+                     input-port
+                     (string-append contents
+                                    (string current-char)))))))
+    (input-helper input-port (make-string 0)))
+  (let ((input-file "./testinputs/testfile.txt"))
+    (cond ((file-exists? input-file)
+           (call-with-input-file input-file input-func)))))
