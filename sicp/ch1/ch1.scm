3cc6cd177f3d06214eb3ec9047417186976ce705
Jeff Gonis <jeffgonis@fastmail.com>
2021-01-24 23:45:14 -0700

    Create a main file for my "load function" to bring in all sources

--- sicp/ch1/ch1.scm
+++ sicp/ch1/ch1.scm
@@ -2,13 +2,6 @@
 ;;"/home/jeff.gonis/Code/gauche/bin/gosh -i -r7 -I /home/jeff.gonis/Code/sicp/sicp"
 ;; (import (gauche base)) to get access to debugging and profiling
 ;; functions
-;; (define (load-func)
-;;   (load "/home/jgonis/code/sicp/sicp/ch1/ch1.scm")
-;;   (load "/home/jgonis/code/sicp/sicp/ch2/ch2.scm")
-;;   (load "/home/jgonis/code/sicp/sicp/libs/fp-compare.scm")
-;;   (load "/home/jgonis/code/sicp/sicp/libs/helpers.scm")
-;;   (load "/home/jgonis/code/sicp/sicp/tests/ch1/ch1Tests.scm")
-;;   (load "/home/jgonis/code/sicp/sicp/tests/ch2/ch2Tests.scm"))
 
 (define-library (ch1 ch1)
   (export ex1-2
--- sicp/ch2/ch2.scm
+++ sicp/ch2/ch2.scm
@@ -1,23 +1,6 @@
 ;;"/home/jeff.gonis/Code/gauche/bin/gosh -i -r7 -I /home/jeff.gonis/Code/sicp/sicp"
 ;; (import (gauche base)) to get access to debugging and profiling
 ;; functions
-;; (define (load-func)
-;;   (load "/home/jgonis/Code/sicp/sicp/ch1/ch1.scm")
-;;   (load "/home/jgonis/Code/sicp/sicp/ch2/ch2.scm")
-;;   (load "/home/jgonis/Code/sicp/sicp/ch2/rationalNumbers.scm")
-;;   (load "/home/jgonis/Code/sicp/sicp/ch2/point.scm")
-;;   (load "/home/jgonis/Code/sicp/sicp/ch2/segment.scm")
-;;   (load "/home/jgonis/Code/sicp/sicp/ch2/rectangle.scm")
-;;   (load "/home/jgonis/Code/sicp/sicp/ch2/rectangleFunctions.scm")
-;;   (load "/home/jgonis/Code/sicp/sicp/ch2/altRectangle.scm")
-;;   (load "/home/jgonis/Code/sicp/sicp/libs/fp-compare.scm")
-;;   (load "/home/jgonis/Code/sicp/sicp/libs/helpers.scm")
-;;   (load "/home/jgonis/Code/sicp/sicp/tests/ch1/ch1Tests.scm")
-;;   (load "/home/jgonis/Code/sicp/sicp/tests/ch2/ch2Tests.scm")
-;;   (load "/home/jgonis/Code/sicp/sicp/tests/ch2/pointTests.scm")
-;;   (load "/home/jgonis/Code/sicp/sicp/tests/ch2/segmentTests.scm")
-;;   (load "/home/jgonis/Code/sicp/sicp/tests/ch2/rectangleTests.scm")
-;;   (load "/home/jgonis/Code/sicp/sicp/tests/ch2/rationalNumberTests.scm"))
 
 (define-library (ch2 ch2)
   (export test-func)
