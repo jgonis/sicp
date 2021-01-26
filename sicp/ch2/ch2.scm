ed502e97cbc30996aaa85986957b7e252ba02ccd
Jeff Gonis <jeffgonis@fastmail.com>
2021-01-26 14:52:05 -0700

    Create alternate implementations for cons, car and cdr

--- sicp/ch2/ch2.scm
+++ sicp/ch2/ch2.scm
@@ -3,11 +3,38 @@
 ;; functions
 
 (define-library (ch2 ch2)
-  (export test-func)
+  (export alt-cons
+	  alt-car
+	  alt-cdr
+	  numeric-cons
+	  numeric-car
+	  numeric-cdr)
   (import (scheme base)
           (scheme write)
 	  (libs fp-compare)
 	  (libs helpers)
 	  (ch2 rationalNumbers))
-  (begin
-    (define (test-func) 1)))
+  (begin    
+    (define (alt-cons x y)
+      (lambda (m) (m x y)))
+    
+    (define (alt-car pair)
+      (pair (lambda (x y) x)))
+    
+    (define (alt-cdr pair)
+      (pair (lambda (x y) y)))
+    
+    (define (numeric-cons x y)
+      (* (expt 2 x) (expt 3 y)))
+    
+    (define (numeric-car pair)
+      (define (iter num counter)
+	(cond ((even? num) counter)
+	      (else (iter (/ num 2) (+ counter 1)))))
+      (iter pair 0))
+    
+    (define (numeric-cdr pair)
+      (define (iter num counter)
+	(cond ((not (= 0 (remainder num 3))) counter)
+	      (else (iter (/ num 3) (+ counter 1)))))
+      (iter pair 0))))
--- sicp/tests/ch2/ch2Tests.scm
+++ sicp/tests/ch2/ch2Tests.scm
@@ -15,17 +15,25 @@
   (begin    
     (define ch2-tests
       (lambda ()
-        (test-func-tests)
 	(rational-number-tests)
 	(point-tests)
 	(segment-tests)
 	(rectangle-tests)
+	(alt-car-cdr-tests)
         ;;If using Gauche scheme, uncomment this line to avoid the
         ;;test count continuing to increase
         (test-runner-reset (test-runner-current))))
 
-    (define (test-func-tests)
-      (test-begin "test-func-tests")
-      (test-equal (test-func) 1)
-      (test-end "test-func-tests"))))
+    (define (alt-car-cdr-tests)
+      (test-begin "alt-car-cdr-tests")
+      (let ((pair (alt-cons 1 2))
+	    (pair2 (alt-cons 0 3))
+	    (pair3 (alt-cons 3 0)))
+	(test-equal 1 (alt-car pair))
+	(test-equal 2 (alt-cdr pair))
+	(test-equal 0 (alt-car pair2))
+	(test-equal 3 (alt-cdr pair2))
+	(test-equal 3 (alt-car pair3))
+	(test-equal 0 (alt-cdr pair3)))
+      (test-end "alt-car-cdr-tests"))))
 
