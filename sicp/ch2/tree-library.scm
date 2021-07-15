838eb5601e4ce8944bb46f886bae72dc1790de88
Jeff Gonis <jeff.gonis@shareworks.com>
2021-07-14 22:54:23 -0600

    Start work on tree functions and associated tests

--- sicp/ch2/tree-library.scm
+++ sicp/ch2/tree-library.scm
@@ -1,6 +1,7 @@
 (define-library (ch2 tree-library)
   (export count-leaves
-	  deep-reverse)
+	  deep-reverse
+	  fringe)
   (import (scheme base))
   (begin
     (define (count-leaves tree)
@@ -8,11 +9,18 @@
 	    ((not (pair? tree)) 1)
 	    (else (+ (count-leaves (car tree))
 		     (count-leaves (cdr tree))))))
+
     (define (deep-reverse tree)
       (define (helper tree result)
 	(cond ((null? tree) result)
 	      ((not (pair? tree)) tree)
 	      (else (helper (cdr tree)
-			     (cons (helper (car tree) '())
-				   result)))))
-      (helper tree '())))) 
+			    (cons (helper (car tree) '())
+				  result)))))
+      (helper tree '()))
+
+    (define (fringe tree)
+      (cond ((null? tree) '())
+	    ((not (pair? tree)) tree)
+	    (else (cons (fringe (car tree))
+			(fringe (cdr tree)))))))) 
