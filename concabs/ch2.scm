25d583a7df978bb0e5b32cfec25a3aa6db23b1ce
Jeff Gonis <jeffgonis@fastmail.com>
2023-09-26 16:06:01 -0600

    continuing work on ch2

--- concabs/ch2.scm
+++ concabs/ch2.scm
@@ -1,7 +1,17 @@
 ;;test comment
 (define-library (concabs ch2)
   (export ex2-1
-          ex2-4)
+          ex2-4
+          fast-ex2-1
+          fast-iter-ex2-1
+          quot
+	  subtract-the-first
+	  sum-of-squares
+	  sum-of-cubes
+	  sum-of-powers
+	  num-digits
+	  number-of-sixes
+	  number-of-digit)
   (import (scheme base)
           (scheme write)
 	  (scheme case-lambda)
@@ -16,6 +26,20 @@
     (define (ex2-1 x y)
       (cond ((= y 0) 1)
 	    (else (* x (ex2-1 x (- y 1))))))
+    (define fast-ex2-1
+      (lambda (x y)
+        (cond ((= y 0) 1)
+              ((even? y) (fast-ex2-1 (* x x) (/ y 2)))
+              (else (* x (fast-ex2-1 x (- y 1)))))))
+    (define fast-iter-ex2-1
+      (lambda (x y)
+        (let loop ((base x)
+                   (exponent y)
+                   (state-var 1))
+          (cond ((= exponent 0) 1)
+                ((= exponent 1) (* state-var base))
+                ((even? exponent) (loop (* base base) (/ exponent 2) state-var))
+                (else (loop base (- exponent 1) (* base state-var)))))))
 
     (define candy-temperature
       (lambda (temp elevation)
@@ -47,6 +71,49 @@
         (cond ((= n 0) 0)
               ((even? n) (* (ex24 (/ n 2)) 4))
               (else (+ (ex24 (- n 1)) (- (+ n n) 1))))))
+
+    (define quot
+      (lambda (n d)
+	(cond ((< n 0) (- (quot (- n) d)))
+	      ((< d 0) (- (quot n (- d))))
+	      ((< n d) 0)
+	      (else (+ 1 (quot (- n d) d))))))
+
+    (define remaind
+      (lambda (n d)
+	(cond ((< n 0) (- (remaind (- n) d)))
+	      ())))
+
+    (define subtract-the-first
+      (lambda (n)
+	(cond ((= n 0) 0)
+	      (else (- (subtract-the-first (- n 1)) n )))))
+
+    (define sum-of-squares
+      (lambda (n)
+	(cond ((= n 0) 0)
+	      (else (+ (* n n) (sum-of-squares (- n 1)))))))
+
+    (define sum-of-cubes
+      (lambda (n)
+	(cond ((= n 0) 0)
+	      (else (+ (* n n n) (sum-of-cubes (- n 1)))))))
+
+    (define sum-of-powers
+      (lambda (n p)
+	(cond ((or (< p 0) (< n 0)) (error "can't have negative arguments"))
+	      ((= n 0) 0)
+	      (else (+ (fast-iter-ex2-1 n p) (sum-of-powers (- n 1) p))))))
+
+    (define num-digits
+      (lambda (n)
+	(cond ((< n 0) (num-digits (- n)))
+	      ((< n 10) 1)
+	      (else (+ 1 (num-digits (quot n 10)))))))
+    (define number-of-sixes
+      (lambda (n)
+	(cond ((< n 0) (number-of-sixes (- n)))
+	      ((= (remainder n 10) 6) (+ 1 (number-of-sixes (quot n 10)))))))
     ))
 
 
--- concabs/libs/quilting.sld
+++ concabs/libs/quilting.sld
@@ -33,7 +33,7 @@
     (filled-triangle 0 1 1 1 1 0))
 
   (define rcross-bb
-    (overlay (overlay (filled-triangle -1/2 -1/2 1/2 -1/2 -1/2 1/2) corner-bb-new)
+    (overlay (overlay (filled-triangle -1/2 -1/2 1/2 -1/2 -1/2 1/2) corner-bb)
              (overlay (overlay (filled-triangle 1 -1 1/2 -1/2 1 0)
                                (filled-triangle 1/2 -1/2 1/2 1/2 1 0))
                       (overlay (filled-triangle -1 1 -1/2 1/2 0 1)
